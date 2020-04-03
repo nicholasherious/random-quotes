2ee61d1d47fa0c26fb8d806da788a33eef47e631
nicholasherious <nherious@gmail.com>
2020-04-03 14:46:03 -0700

    Updates

---
+++ precache-manifest.526f046f561c3157cafca57309e4807a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f9755204137479a22378b1be2e6b83",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "41eec88d4f5bb35bb740",
+    "url": "/random-quotes/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "649d68b2b2ac41b14826",
+    "url": "/random-quotes/static/css/main.6da85b62.chunk.css"
+  },
+  {
+    "revision": "41eec88d4f5bb35bb740",
+    "url": "/random-quotes/static/js/2.de8fa6eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quotes/static/js/2.de8fa6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649d68b2b2ac41b14826",
+    "url": "/random-quotes/static/js/main.755cf988.chunk.js"
+  },
+  {
+    "revision": "ce0ab77c84e3e2817825",
+    "url": "/random-quotes/static/js/runtime-main.05a610f2.js"
+  }
+]);
